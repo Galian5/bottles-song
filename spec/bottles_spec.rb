529b86e5f7e448947a859e5b47593e044ed46eae
Galian <galian5@wp.pl>
2018-07-07 10:06:01 +0200

    pass first test

--- spec/bottles_spec.rb
+++ spec/bottles_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 require_relative '../lib/bottles'
 
 RSpec.describe Bottles, "next row" do
-  context "runs new werse" do
+  context "runs new verse" do
     it "should return right text" do
 
       expect(Bottles.new.verse(99)).to eq '99 bottles of beer on the wall, 99 bottles of beer.
