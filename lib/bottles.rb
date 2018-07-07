9b52f47f7a575d09d657c557917e5ffeaf04cb91
Galian <galian5@wp.pl>
2018-07-07 11:30:33 +0200

    pass test 6

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,5 +1,3 @@
-require 'rubygems'
-require 'active_support/core_ext/string'
 
 class Bottles
   def verse(amount)
@@ -10,6 +8,24 @@ class Bottles
       'No more bottles of beer on the wall, no more bottles of beer.
  Go to the store and buy some more, 99 bottles of beer on the wall.'
     end
+  end
+
+  def verses(x, y)
+    "#{x} bottles of beer on the wall, #{x} bottles of beer.
+ Take one down and pass it around, #{x - 1} bottles of beer on the wall.
+ #{y} bottles of beer on the wall, #{y} bottles of beer.
+ Take one down and pass it around, #{y - 1} bottles of beer on the wall."
+
+  end
+
+  private
+  def pluralize(number, word)
+    if number > 1
+      return number.to_s+' '+word+'s'
+    elsif number == 0
+      return 'no more bottles'
+    end
+    number.to_s+' '+word
   end
 
 end
