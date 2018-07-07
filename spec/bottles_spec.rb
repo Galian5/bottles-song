c916ba2e292cfa3d60dc6c67f5e345b1425680e0
Galian <galian5@wp.pl>
2018-07-07 10:13:19 +0200

    add two more test, which are passed

--- spec/bottles_spec.rb
+++ spec/bottles_spec.rb
@@ -5,10 +5,28 @@ RSpec.describe Bottles, "next row" do
   context "runs new verse" do
     it "should return right text" do
 
-      expect(Bottles.new.verse(99)).to eq '99 bottles of beer on the wall, 99 bottles of beer.
-      Take one down and pass it around, 98 bottles of beer on the wall.'
+      expect(Bottles.new.verse(99)).to eq "99 bottles of beer on the wall, 99 bottles of beer.
+      Take one down and pass it around, 98 bottles of beer on the wall."
     end
   end
+end
 
+RSpec.describe Bottles, "next row" do
+  context "runs new verse" do
+    it "should return right text" do
 
+      expect(Bottles.new.verse(98)).to eq "98 bottles of beer on the wall, 98 bottles of beer.
+      Take one down and pass it around, 97 bottles of beer on the wall."
+    end
+  end
+end
+
+RSpec.describe Bottles, "next row" do
+  context "runs new verse" do
+    it "should return right text" do
+
+      expect(Bottles.new.verse(97)).to eq "97 bottles of beer on the wall, 97 bottles of beer.
+      Take one down and pass it around, 96 bottles of beer on the wall."
+    end
+  end
 end
