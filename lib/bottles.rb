ba75d14afd8677fb7fa5c2b85aa4b871774492c2
Galian <galian5@wp.pl>
2018-07-07 10:25:40 +0200

    rubocop refactor

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,12 +1,12 @@
 class Bottles
   def verse(amount)
-    if amount==1
+    if amount == 1
       '1 bottle of beer on the wall, 1 bottle of beer.
       Take it down and pass it around, no more bottles of beer on the wall.'
 
     else
       "#{amount} bottles of beer on the wall, #{amount} bottles of beer.
-      Take one down and pass it around, #{amount-1} bottles of beer on the wall."
+      Take one down and pass it around, #{amount - 1} bottles of beer on the wall."
 
     end
   end
--- spec/bottles_spec.rb
+++ spec/bottles_spec.rb
@@ -1,42 +1,42 @@
 require 'spec_helper'
 require_relative '../lib/bottles'
 
-RSpec.describe Bottles, "next row" do
-  context "runs new verse" do
-    it "should return right text" do
+RSpec.describe Bottles, 'next row' do
+  context 'runs new verse' do
+    it 'should return right text' do
 
-      expect(Bottles.new.verse(99)).to eq "99 bottles of beer on the wall, 99 bottles of beer.
-      Take one down and pass it around, 98 bottles of beer on the wall."
+      expect(Bottles.new.verse(99)).to eq '99 bottles of beer on the wall, 99 bottles of beer.
+      Take one down and pass it around, 98 bottles of beer on the wall.'
     end
   end
 end
 
-RSpec.describe Bottles, "next row" do
-  context "runs new verse" do
-    it "should return right text" do
+RSpec.describe Bottles, 'next row' do
+  context 'runs new verse' do
+    it 'should return right text' do
 
-      expect(Bottles.new.verse(98)).to eq "98 bottles of beer on the wall, 98 bottles of beer.
-      Take one down and pass it around, 97 bottles of beer on the wall."
+      expect(Bottles.new.verse(98)).to eq '98 bottles of beer on the wall, 98 bottles of beer.
+      Take one down and pass it around, 97 bottles of beer on the wall.'
     end
   end
 end
 
-RSpec.describe Bottles, "next row" do
-  context "runs new verse" do
-    it "should return right text" do
+RSpec.describe Bottles, 'next row' do
+  context 'runs new verse' do
+    it 'should return right text' do
 
-      expect(Bottles.new.verse(97)).to eq "97 bottles of beer on the wall, 97 bottles of beer.
-      Take one down and pass it around, 96 bottles of beer on the wall."
+      expect(Bottles.new.verse(97)).to eq '97 bottles of beer on the wall, 97 bottles of beer.
+      Take one down and pass it around, 96 bottles of beer on the wall.'
     end
   end
 end
 
-RSpec.describe Bottles, "next row" do
-  context "runs new verse" do
-    it "should return right text" do
+RSpec.describe Bottles, 'next row' do
+  context 'runs new verse' do
+    it 'should return right text' do
 
-      expect(Bottles.new.verse(1)).to eq "1 bottle of beer on the wall, 1 bottle of beer.
-      Take it down and pass it around, no more bottles of beer on the wall."
+      expect(Bottles.new.verse(1)).to eq '1 bottle of beer on the wall, 1 bottle of beer.
+      Take it down and pass it around, no more bottles of beer on the wall.'
     end
   end
 end
